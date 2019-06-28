9de696c4a89bc2a6ffafae8524f7ed67bd64d2c3
Nikita Timofeev <stariy95@gmail.com>
2019-06-28 17:31:21 +0300

    native-image settings for Windows

---
+++ src/main/java/org/jline/utils/Signals.java
@@ -0,0 +1,26 @@
+package org.jline.utils;
+
+/**
+ * Stub implementation of JLine Signals helpers.
+ */
+public final class Signals {
+
+    private Signals() {
+    }
+
+    public static Object register(String name, Runnable handler) {
+        return null;
+    }
+
+    public static Object register(String name, final Runnable handler, ClassLoader loader) {
+        return null;
+    }
+
+    public static Object registerDefault(String name) {
+        return null;
+    }
+
+    public static void unregister(String name, Object previous) {
+    }
+
+}
