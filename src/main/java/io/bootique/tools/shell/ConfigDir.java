f926246841aac2cde87b67dcfabb7dfdfcc7f4fc
Nikita Timofeev <stariy95@gmail.com>
2019-07-02 16:28:36 +0300

    Native-image support

--- src/main/java/io/bootique/tools/shell/ConfigDir.java
+++ src/main/java/io/bootique/tools/shell/ConfigDir.java
@@ -1,4 +1,8 @@
 package io.bootique.tools.shell;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
 public @interface ConfigDir {
 }
