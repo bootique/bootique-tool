9f9ab2f09848a8c4ab88a05cce26fd2f22cb3e72
Arseni Bulatski <ancarseni@gmail.com>
2019-09-06 11:04:35 +0300

    Try to build native image in Travis

---
+++ mac.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+curl -OL https://github.com/oracle/graal/releases/download/vm-19.1.1/graalvm-ce-darwin-amd64-19.1.1.tar.gz
+tar zxf graalvm-ce-darwin-amd64-19.1.1.tar.gz
+sudo mv graalvm-ce-19.1.1 /Library/Java/JavaVirtualMachines
+/usr/libexec/java_home -v 1.8
+export PATH=/Library/Java/JavaVirtualMachines/graalvm-ce-19.1.1/Contents/Home/bin:$PATH
+/Library/Java/JavaVirtualMachines/graalvm-ce-19.1.1/Contents/Home/bin/gu install native-image
+mvn clean package -Pnative-image
+mvn package -P assembly
