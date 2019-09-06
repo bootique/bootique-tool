6d9ead705cc2537377a80922d3c57ae084d9a537
Arseni Bulatski <ancarseni@gmail.com>
2019-09-06 11:16:32 +0300

    Preparing mac

--- mac.sh
+++ mac.sh
@@ -4,7 +4,8 @@ tar zxf graalvm-ce-darwin-amd64-19.1.1.tar.gz
 sudo mv graalvm-ce-19.1.1 /Library/Java/JavaVirtualMachines
 /usr/libexec/java_home -v 1.8
 export PATH=/Library/Java/JavaVirtualMachines/graalvm-ce-19.1.1/Contents/Home/bin:$PATH
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/graalvm-ce-19.1.1/Contents/Home
 /Library/Java/JavaVirtualMachines/graalvm-ce-19.1.1/Contents/Home/bin/gu install native-image
 java -version
-#mvn clean package -Pnative-image
-#mvn package -P assembly
+mvn clean package -Pnative-image
+mvn package -P assembly
